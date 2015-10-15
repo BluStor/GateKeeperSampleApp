4d93f0cd51b707969602a432aa9e33b16f694e37
Jeremy Neander <subjectego@gmail.com>
2015-10-15 10:35:20 -0500

    Start Authentication Activity from delayed Thread

--- app/src/main/java/co/blustor/gatekeeper/SplashActivity.java
+++ app/src/main/java/co/blustor/gatekeeper/SplashActivity.java
@@ -3,13 +3,35 @@ package co.blustor.gatekeeper;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 public class SplashActivity extends Activity {
+    public final String TAG = SplashActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent(this, AuthenticationActivity.class);
-        startActivity(intent);
+
+        Thread thread = new Thread(startNextActivity);
+        thread.start();
     }
-};
+
+    private Runnable startNextActivity = new Runnable() {
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            try {
+                startActivity(new Intent(SplashActivity.this, AuthenticationActivity.class));
+                finish();
+            } catch (Exception e) {
+                Log.e(TAG, "something went wrong", e);
+                finish();
+            }
+        }
+    };
+}
