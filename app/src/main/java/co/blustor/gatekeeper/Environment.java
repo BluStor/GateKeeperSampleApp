c5eef962f9b0e36104363eab719342860a2b4ca5
Jeremy Neander <subjectego@gmail.com>
2015-10-16 09:02:56 -0500

    Add Faces.ndf; Extract Environment class

---
+++ app/src/main/java/co/blustor/gatekeeper/Environment.java
@@ -0,0 +1,80 @@
+package co.blustor.gatekeeper;
+
+import android.content.Context;
+import android.os.AsyncTask;
+
+import com.neurotec.plugins.NDataFileManager;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Environment {
+    public static String TAG = Environment.class.getSimpleName();
+
+    public enum Status {
+        OK,
+        PREPARING,
+        NO_LICENSE
+    }
+
+    private static Environment mInstance;
+    private final Context mContext;
+
+    private Environment(Context context) {
+        mContext = context;
+    }
+
+    public static Environment getInstance(Context context) {
+        if (mInstance == null) {
+            mInstance = new Environment(context);
+        }
+        return mInstance;
+    }
+
+    public AsyncTask<Void, Void, Status> initialize(final InitializationListener listener) {
+        AsyncTask<Void, Void, Status> asyncTask = new AsyncTask<Void, Void, Status>() {
+            @Override
+            protected Environment.Status doInBackground(Void... params) {
+                listener.onStatusChanged(Environment.Status.PREPARING);
+                ensureDataFilesExist();
+                return Environment.Status.OK;
+            }
+
+            @Override
+            protected void onPostExecute(Environment.Status status) {
+                super.onPostExecute(status);
+                listener.onComplete(Environment.Status.OK);
+            }
+        };
+        asyncTask.execute();
+        return asyncTask;
+    }
+
+    public interface InitializationListener {
+        void onStatusChanged(Status status);
+
+        void onComplete(Status status);
+    }
+
+    private void ensureDataFilesExist() {
+        File facesFile = new File(mContext.getFilesDir(), "Faces.ndf");
+        if (!facesFile.exists()) {
+            try {
+                InputStream is = mContext.getAssets().open("Faces.ndf");
+                int size = is.available();
+                byte[] buffer = new byte[size];
+                is.read(buffer);
+                is.close();
+
+                FileOutputStream fos = new FileOutputStream(facesFile);
+                fos.write(buffer);
+                fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        NDataFileManager.getInstance().addFile(facesFile.getAbsolutePath());
+    }
+}
--- app/src/main/java/co/blustor/gatekeeper/SplashActivity.java
+++ app/src/main/java/co/blustor/gatekeeper/SplashActivity.java
@@ -3,9 +3,8 @@ package co.blustor.gatekeeper;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 
-public class SplashActivity extends Activity {
+public class SplashActivity extends Activity implements Environment.InitializationListener {
     public final String TAG = SplashActivity.class.getSimpleName();
 
     @Override
@@ -13,25 +12,17 @@ public class SplashActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Thread thread = new Thread(startNextActivity);
-        thread.start();
+        Environment env = Environment.getInstance(this);
+        env.initialize(this);
     }
 
-    private Runnable startNextActivity = new Runnable() {
-        @Override
-        public void run() {
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            try {
-                startActivity(new Intent(SplashActivity.this, AuthenticationActivity.class));
-                finish();
-            } catch (Exception e) {
-                Log.e(TAG, "something went wrong", e);
-                finish();
-            }
-        }
-    };
+    @Override
+    public void onStatusChanged(Environment.Status status) {
+    }
+
+    @Override
+    public void onComplete(Environment.Status status) {
+        startActivity(new Intent(SplashActivity.this, AuthenticationActivity.class));
+        finish();
+    }
 }
