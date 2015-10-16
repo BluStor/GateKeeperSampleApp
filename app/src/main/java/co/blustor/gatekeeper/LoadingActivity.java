8ceb00191ee3dfb3cebf04b603bde4a1b3d1f682
Jeremy Neander <subjectego@gmail.com>
2015-10-16 09:04:29 -0500

    Rename Splash Activity to Loading Activity

--- app/src/main/java/co/blustor/gatekeeper/LoadingActivity.java
+++ app/src/main/java/co/blustor/gatekeeper/LoadingActivity.java
@@ -4,13 +4,13 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 
-public class SplashActivity extends Activity implements Environment.InitializationListener {
-    public final String TAG = SplashActivity.class.getSimpleName();
+public class LoadingActivity extends Activity implements Environment.InitializationListener {
+    public final String TAG = LoadingActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_loading);
 
         Environment env = Environment.getInstance(this);
         env.initialize(this);
@@ -22,7 +22,7 @@ public class SplashActivity extends Activity implements Environment.Initializati
 
     @Override
     public void onComplete(Environment.Status status) {
-        startActivity(new Intent(SplashActivity.this, AuthenticationActivity.class));
+        startActivity(new Intent(this, AuthenticationActivity.class));
         finish();
     }
 }
