c223304dea2cbc9a2b4f5a3241128f59b4f6251a
Jason <jgoodman@8thlight.com>
2015-10-27 14:55:18 -0500

    Extract FaceAuthenticator interface and LocalFaceAuthenticator class.

--- app/src/main/java/co/blustor/gatekeeper/activities/AuthenticationActivity.java
+++ app/src/main/java/co/blustor/gatekeeper/activities/AuthenticationActivity.java
@@ -10,9 +10,7 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import com.neurotec.biometrics.NBiometricCaptureOption;
-import com.neurotec.biometrics.NBiometricOperation;
 import com.neurotec.biometrics.NBiometricStatus;
-import com.neurotec.biometrics.NBiometricTask;
 import com.neurotec.biometrics.NFace;
 import com.neurotec.biometrics.NSubject;
 import com.neurotec.biometrics.client.NBiometricClient;
@@ -23,10 +21,10 @@ import com.neurotec.devices.NDeviceManager;
 import com.neurotec.devices.NDeviceType;
 import com.neurotec.util.concurrent.CompletionHandler;
 
-import java.io.IOException;
 import java.util.EnumSet;
 
 import co.blustor.gatekeeper.R;
+import co.blustor.gatekeeper.authentication.LocalFaceAuthenticator;
 import co.blustor.gatekeeper.data.Datastore;
 import co.blustor.gatekeeper.data.DroidDatastore;
 
@@ -36,16 +34,17 @@ public class AuthenticationActivity extends Activity {
     private NFaceView mFaceView;
     private Button mCaptureButton;
 
-    private Datastore mDatastore;
     private NBiometricClient mBiometricClient;
+    private LocalFaceAuthenticator mLocalFaceAuthenticator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mDatastore = DroidDatastore.getInstance(this);
         setContentView(R.layout.activity_authentication);
         initializeViews();
         initializeClient();
+        Datastore datastore = DroidDatastore.getInstance(this);
+        mLocalFaceAuthenticator = new LocalFaceAuthenticator(datastore);
     }
 
     @Override
@@ -109,45 +108,11 @@ public class AuthenticationActivity extends Activity {
         @Override
         public void completed(final NBiometricStatus result, NSubject capturedSubject) {
             if (result == NBiometricStatus.OK) {
-                try {
-                    NSubject storedSubject = mDatastore.getTemplate();
-
-                    NBiometricTask enrollTask = mBiometricClient.createTask(EnumSet.of(NBiometricOperation.ENROLL), storedSubject);
-                    NBiometricTask identifyTask = mBiometricClient.createTask(EnumSet.of(NBiometricOperation.IDENTIFY), capturedSubject);
-
-                    mBiometricClient.performTask(enrollTask, NBiometricOperation.ENROLL, new CompletionHandler<NBiometricTask, NBiometricOperation>() {
-                        @Override
-                        public void completed(NBiometricTask result, NBiometricOperation nBiometricOperation) {
-                            Log.e(TAG, "Enrollment completed with status: " + result.getStatus());
-                        }
-
-                        @Override
-                        public void failed(Throwable throwable, NBiometricOperation nBiometricOperation) {
-                            Log.e(TAG, "Enrollment failed.");
-                        }
-                    });
-
-                    mBiometricClient.performTask(identifyTask, NBiometricOperation.IDENTIFY, new CompletionHandler<NBiometricTask, NBiometricOperation>() {
-                        @Override
-                        public void completed(NBiometricTask result, NBiometricOperation nBiometricOperation) {
-                            if (result.getStatus() == NBiometricStatus.OK) {
-                                Log.e(TAG, "You are authenticated.");
-                                startActivity(new Intent(AuthenticationActivity.this, FileBrowserActivity.class));
-                            } else {
-                                Log.e(TAG, "Authentication failed.  Result Status: " + result.getStatus());
-                            }
-                        }
-
-                        @Override
-                        public void failed(Throwable throwable, NBiometricOperation nBiometricOperation) {
-
-                        }
-                    });
-
-
-
-                } catch (IOException e) {
-                    Log.e(TAG, "Failed to load template.  " + e.toString());
+                if(mLocalFaceAuthenticator.authenticate(capturedSubject)) {
+                    Log.e(TAG, "Authenticated!");
+                    startActivity(new Intent(AuthenticationActivity.this, FileBrowserActivity.class));
+                } else {
+                    Log.e(TAG, "Auth failed...");
                 }
             } else {
                 showMessage(R.string.bio_status_not_ok);
---
+++ app/src/main/java/co/blustor/gatekeeper/authentication/LocalFaceAuthenticator.java
@@ -0,0 +1,46 @@
+package co.blustor.gatekeeper.authentication;
+
+
+import android.util.Log;
+
+import com.neurotec.biometrics.NBiometricOperation;
+import com.neurotec.biometrics.NBiometricStatus;
+import com.neurotec.biometrics.NBiometricTask;
+import com.neurotec.biometrics.NSubject;
+import com.neurotec.biometrics.client.NBiometricClient;
+
+import java.io.IOException;
+import java.util.EnumSet;
+
+import co.blustor.gatekeeper.data.Datastore;
+
+public class LocalFaceAuthenticator {
+    private static final String TAG = "LocalFaceAuthenticator";
+
+    private Datastore mDataStore;
+
+    public LocalFaceAuthenticator(Datastore datastore) {
+        mDataStore = datastore;
+    }
+
+    public boolean authenticate(NSubject testSubject) {
+        NSubject enrolledSubject = null;
+        try {
+            enrolledSubject = mDataStore.getTemplate();
+        } catch (IOException e) {
+            Log.e(TAG, "Failed to get enrolled template from Datastore.");
+            e.printStackTrace();
+            return false;
+        }
+
+        NBiometricClient client = new NBiometricClient();
+
+        NBiometricTask enrollTask = client.createTask(EnumSet.of(NBiometricOperation.ENROLL), enrolledSubject);
+        NBiometricTask identifyTask = client.createTask(EnumSet.of(NBiometricOperation.IDENTIFY), testSubject);
+
+        client.performTask(enrollTask);
+        client.performTask(identifyTask);
+
+        return identifyTask.getStatus() == NBiometricStatus.OK;
+    }
+}
