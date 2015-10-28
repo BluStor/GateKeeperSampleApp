2204d911fb8265dee161ce9efd9b7d9d3c0c024e
Jeremy Neander <subjectego@gmail.com>
2015-10-28 15:47:08 -0500

    Remove IOConnection interface

--- app/src/main/java/co/blustor/gatekeeper/data/IOConnection.java
+++
@@ -1,8 +0,0 @@
-package co.blustor.gatekeeper.data;
-
-import java.io.IOException;
-
-public interface IOConnection {
-    void open() throws IOException;
-    void close() throws IOException;
-}
--- app/src/main/java/co/blustor/gatekeeper/data/RemoteFilestoreClient.java
+++ app/src/main/java/co/blustor/gatekeeper/data/RemoteFilestoreClient.java
@@ -4,8 +4,10 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
-public interface RemoteFilestoreClient extends IOConnection {
+public interface RemoteFilestoreClient {
     List<VaultFile> listFiles(String targetPath) throws IOException;
     File downloadFile(VaultFile vaultFile) throws IOException;
     String getRootPath();
+    void open() throws IOException;
+    void close() throws IOException;
 }
