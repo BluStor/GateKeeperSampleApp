1458ba0d5636d2f0dfae072226c05ab1cad8d118
Jason <jgoodman@8thlight.com>
2015-10-31 19:26:55 -0500

    Add threading to SerialPortFTPClient.
    Basic working listFiles with threading.

--- app/src/ftp/java/co/blustor/gatekeeper/bluetooth/SerialPortFTPClient.java
+++ app/src/ftp/java/co/blustor/gatekeeper/bluetooth/SerialPortFTPClient.java
@@ -5,6 +5,7 @@ import android.util.Log;
 
 import org.apache.commons.net.ftp.FTPFile;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -25,11 +26,20 @@ public class SerialPortFTPClient {
         String cmd = "LIST " + pathname + "*" + "\r\n";
         byte[] bytes = cmd.getBytes(StandardCharsets.US_ASCII);
         mSerialPortMultiplexer.write(bytes, COMMAND_CHANNEL);
-        byte[] line;
-        while((line = mSerialPortMultiplexer.readLine(DATA_CHANNEL)).length > 0) {
 
-            Log.e(TAG, new String(line));
-        }
+        ReadDataThread readDataThread = new ReadDataThread(mSerialPortMultiplexer);
+        Thread t = new Thread(readDataThread);
+        byte[] line = mSerialPortMultiplexer.readLine(COMMAND_CHANNEL);
+        String command = new String(line);
+        Log.e(TAG, command);
+        t.start();
+        line = mSerialPortMultiplexer.readLine(COMMAND_CHANNEL);
+        command = new String(line);
+        Log.e(TAG, command);
+        t.interrupt();
+        String data = new String(readDataThread.getData());
+        Log.e(TAG, data);
+
         return new FTPFile[1];
     }
 
@@ -62,5 +72,32 @@ public class SerialPortFTPClient {
 
     public void disconnect() {
         // Done
+    }
+
+    private class ReadDataThread implements Runnable {
+        private ByteArrayOutputStream data;
+        private SerialPortMultiplexer multiplexer;
+
+        public ReadDataThread(SerialPortMultiplexer serialPortMultiplexer) {
+            data = new ByteArrayOutputStream();
+            multiplexer = serialPortMultiplexer;
+        }
+
+        public void run() {
+            byte[] b = new byte[1];
+            while(!Thread.interrupted()) {
+                try {
+                    multiplexer.read(b, DATA_CHANNEL);
+                    data.write(b[0]);
+                } catch (IOException e) {
+                    Log.e(TAG, "IOException in ReadDataThread while trying to read byte from DataChannel.");
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        public byte[] getData() {
+            return data.toByteArray();
+        }
     }
 }
