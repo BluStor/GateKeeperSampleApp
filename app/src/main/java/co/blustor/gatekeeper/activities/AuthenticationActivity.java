f772768dcc299dedd120ee85cbd7a764a06434ac
Jeremy Neander <subjectego@gmail.com>
2015-11-03 12:43:25 -0600

    Derive Enrollment and Authentication activities from Abstract Activity

--- app/src/main/java/co/blustor/gatekeeper/activities/AuthenticationActivity.java
+++ app/src/main/java/co/blustor/gatekeeper/activities/AuthenticationActivity.java
@@ -1,157 +1,42 @@
 package co.blustor.gatekeeper.activities;
 
-import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
-import com.neurotec.biometrics.NBiometricCaptureOption;
-import com.neurotec.biometrics.NBiometricStatus;
-import com.neurotec.biometrics.NFace;
 import com.neurotec.biometrics.NSubject;
-import com.neurotec.biometrics.client.NBiometricClient;
-import com.neurotec.biometrics.view.NFaceView;
-import com.neurotec.devices.NCamera;
-import com.neurotec.devices.NDevice;
-import com.neurotec.devices.NDeviceManager;
-import com.neurotec.devices.NDeviceType;
-import com.neurotec.util.concurrent.CompletionHandler;
-
-import java.util.EnumSet;
 
 import co.blustor.gatekeeper.R;
 import co.blustor.gatekeeper.authentication.LocalFaceAuthenticator;
 import co.blustor.gatekeeper.data.Datastore;
 import co.blustor.gatekeeper.data.DroidDatastore;
 
-public class AuthenticationActivity extends Activity {
-    public String TAG = AuthenticationActivity.class.getSimpleName();
+public class AuthenticationActivity extends FaceAuthActivity {
+    public static final String TAG = AuthenticationActivity.class.getSimpleName();
 
-    private NFaceView mFaceView;
-    private Button mCaptureButton;
-
-    private NBiometricClient mBiometricClient;
     private LocalFaceAuthenticator mLocalFaceAuthenticator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_authentication);
-        initializeViews();
-        initializeClient();
+        setCaptureButtonText(R.string.authenticate);
         Datastore datastore = DroidDatastore.getInstance(this);
         mLocalFaceAuthenticator = new LocalFaceAuthenticator(datastore);
     }
 
     @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        stopCapturing();
-        if (mBiometricClient != null) {
-            mBiometricClient.cancel();
-            mBiometricClient.dispose();
-            mBiometricClient = null;
+    protected void onCaptureSuccess(NSubject subject) {
+        if (mLocalFaceAuthenticator.authenticate(subject)) {
+            showMessage(R.string.authentication_result_success);
+            startActivity(new Intent(AuthenticationActivity.this, AppLauncherActivity.class));
+            finish();
+        } else {
+            showMessage(R.string.authentication_result_failure);
+            startCapturing();
         }
     }
 
-    private void initializeViews() {
-        mFaceView = (NFaceView) findViewById(R.id.camera_view);
-        mCaptureButton = (Button) findViewById(R.id.capture);
-        mCaptureButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                stopCapturing();
-            }
-        });
-    }
-
-    private void initializeClient() {
-        mBiometricClient = new NBiometricClient();
-        mBiometricClient.setUseDeviceManager(true);
-        NDeviceManager deviceManager = mBiometricClient.getDeviceManager();
-        deviceManager.setDeviceTypes(EnumSet.of(NDeviceType.CAMERA));
-        mBiometricClient.initialize();
-        startCapturing();
-    }
-
-    private void startCapturing() {
-        NSubject subject = new NSubject();
-        NFace face = new NFace();
-        face.setCaptureOptions(EnumSet.of(NBiometricCaptureOption.MANUAL));
-        mFaceView.setFace(face);
-        subject.getFaces().add(face);
-
-        NCamera camera = (NCamera) connectCamera(mBiometricClient.getDeviceManager());
-        mBiometricClient.setFaceCaptureDevice(camera);
-        mBiometricClient.capture(subject, subject, completionHandler);
-        showMessage(R.string.turn_camera_to_face);
-    }
-
-    private void stopCapturing() {
-        mBiometricClient.force();
-    }
-
-    private void startResultsActivity(EnrollmentResultActivity.Result result) {
-        Intent resultIntent = new Intent(AuthenticationActivity.this, EnrollmentResultActivity.class);
-        resultIntent.putExtra(EnrollmentResultActivity.RESULT_KEY, result);
-        startActivity(resultIntent);
-        finish();
-    }
-
-
-
-    private CompletionHandler<NBiometricStatus, NSubject> completionHandler = new CompletionHandler<NBiometricStatus, NSubject>() {
-        @Override
-        public void completed(final NBiometricStatus result, NSubject capturedSubject) {
-            if (result == NBiometricStatus.OK) {
-                if(mLocalFaceAuthenticator.authenticate(capturedSubject)) {
-                    showMessage(R.string.authentication_result_success);
-                    startActivity(new Intent(AuthenticationActivity.this, AppLauncherActivity.class));
-                    finish();
-                } else {
-                    showMessage(R.string.authentication_result_failure);
-                    startCapturing();
-                }
-            } else {
-                showMessage(R.string.bio_status_not_ok);
-                startCapturing();
-            }
-        }
-
-        @Override
-        public void failed(Throwable exc, NSubject subject) {
-            Log.e(TAG, "Biometric Capture Failed!");
-            exc.printStackTrace();
-            startResultsActivity(EnrollmentResultActivity.Result.CAPTURE_FAILED);
-        }
-    };
-
-    private NDevice connectCamera(NDeviceManager deviceManager) {
-        NDeviceManager.DeviceCollection devices = deviceManager.getDevices();
-        int count = devices.size();
-        if (count == 0) {
-            throw new RuntimeException("No cameras found, exiting!");
-        }
-        for (int i = 0; i < count; i++) {
-            NDevice device = devices.get(i);
-            if (device.getDisplayName().contains("Front")) {
-                return device;
-            }
-        }
-        return devices.get(0);
-    }
-
-    private void showMessage(final int messageResource) {
-        final Context context = this;
-        runOnUiThread(new Runnable() {
-            public void run() {
-                Log.i(TAG, getString(messageResource));
-                Toast.makeText(context, messageResource, Toast.LENGTH_LONG).show();
-            }
-        });
+    @Override
+    protected void onCaptureFailure(Throwable e) {
+        startResultsActivity(EnrollmentResultActivity.Result.CAPTURE_FAILED);
     }
 }
---
+++ app/src/main/java/co/blustor/gatekeeper/activities/FaceAuthActivity.java
@@ -0,0 +1,154 @@
+package co.blustor.gatekeeper.activities;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.neurotec.biometrics.NBiometricCaptureOption;
+import com.neurotec.biometrics.NBiometricStatus;
+import com.neurotec.biometrics.NFace;
+import com.neurotec.biometrics.NSubject;
+import com.neurotec.biometrics.client.NBiometricClient;
+import com.neurotec.biometrics.view.NFaceView;
+import com.neurotec.devices.NCamera;
+import com.neurotec.devices.NDevice;
+import com.neurotec.devices.NDeviceManager;
+import com.neurotec.devices.NDeviceType;
+import com.neurotec.util.concurrent.CompletionHandler;
+
+import java.util.EnumSet;
+
+import co.blustor.gatekeeper.R;
+
+public abstract class FaceAuthActivity extends Activity {
+    public static final String TAG = FaceAuthActivity.class.getSimpleName();
+
+    private NFaceView mFaceView;
+    private Button mCaptureButton;
+
+    private NBiometricClient mBiometricClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_face_auth);
+        initializeViews();
+        initializeClient();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        stopCapturing();
+        if (mBiometricClient != null) {
+            mBiometricClient.cancel();
+            mBiometricClient.dispose();
+            mBiometricClient = null;
+        }
+    }
+
+    private void initializeViews() {
+        mFaceView = (NFaceView) findViewById(R.id.camera_view);
+        mCaptureButton = (Button) findViewById(R.id.capture);
+        mCaptureButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopCapturing();
+            }
+        });
+    }
+
+    private void initializeClient() {
+        mBiometricClient = new NBiometricClient();
+        mBiometricClient.setUseDeviceManager(true);
+        NDeviceManager deviceManager = mBiometricClient.getDeviceManager();
+        deviceManager.setDeviceTypes(EnumSet.of(NDeviceType.CAMERA));
+        mBiometricClient.initialize();
+        startCapturing();
+    }
+
+    protected void setCaptureButtonText(int textResource) {
+        mCaptureButton.setText(textResource);
+    }
+
+    protected void startCapturing() {
+        NSubject subject = new NSubject();
+        NFace face = new NFace();
+        face.setCaptureOptions(EnumSet.of(NBiometricCaptureOption.MANUAL));
+        mFaceView.setFace(face);
+        subject.getFaces().add(face);
+
+        NCamera camera = (NCamera) connectCamera(mBiometricClient.getDeviceManager());
+        mBiometricClient.setFaceCaptureDevice(camera);
+        mBiometricClient.capture(subject, subject, createCompletionHandler());
+        showMessage(R.string.turn_camera_to_face);
+    }
+
+    protected void stopCapturing() {
+        mBiometricClient.force();
+    }
+
+    protected void startResultsActivity(EnrollmentResultActivity.Result result) {
+        Intent resultIntent = new Intent(this, EnrollmentResultActivity.class);
+        resultIntent.putExtra(EnrollmentResultActivity.RESULT_KEY, result);
+        startActivity(resultIntent);
+        finish();
+    }
+
+    protected CompletionHandler<NBiometricStatus, NSubject> createCompletionHandler() {
+        final FaceAuthActivity activity = this;
+        return new CompletionHandler<NBiometricStatus, NSubject>() {
+            @Override
+            public void completed(NBiometricStatus status, NSubject subject) {
+                if (status == NBiometricStatus.OK) {
+                    Log.i(TAG, "Biometric Capture was successful");
+                    activity.onCaptureSuccess(subject);
+                } else {
+                    Log.i(TAG, "Biometric Capture was not successful");
+                    showMessage(R.string.bio_status_not_ok);
+                    startCapturing();
+                }
+            }
+
+            @Override
+            public void failed(Throwable e, NSubject subject) {
+                Log.e(TAG, "Biometric Capture failed", e);
+                activity.onCaptureFailure(e);
+            }
+        };
+    }
+
+    private NDevice connectCamera(NDeviceManager deviceManager) {
+        NDeviceManager.DeviceCollection devices = deviceManager.getDevices();
+        int count = devices.size();
+        if (count == 0) {
+            throw new RuntimeException("No cameras found, exiting!");
+        }
+        for (int i = 0; i < count; i++) {
+            NDevice device = devices.get(i);
+            if (device.getDisplayName().contains("Front")) {
+                return device;
+            }
+        }
+        return devices.get(0);
+    }
+
+    protected void showMessage(final int messageResource) {
+        final Context context = this;
+        runOnUiThread(new Runnable() {
+            public void run() {
+                Log.i(TAG, getString(messageResource));
+                Toast.makeText(context, messageResource, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    protected abstract void onCaptureSuccess(NSubject subject);
+
+    protected abstract void onCaptureFailure(Throwable e);
+}
