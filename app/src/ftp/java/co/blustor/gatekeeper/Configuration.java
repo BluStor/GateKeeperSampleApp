d8b99571e2bd50a8fef5a0eed6b92dbbf62dddc4
Jason <jgoodman@8thlight.com>
2015-11-05 09:37:30 -0600

    Extract SerialPortFTPClientFactory from Configuration.

--- app/src/ftp/java/co/blustor/gatekeeper/Configuration.java
+++ app/src/ftp/java/co/blustor/gatekeeper/Configuration.java
@@ -1,19 +1,11 @@
 package co.blustor.gatekeeper;
 
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothSocket;
+import android.util.Log;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.Set;
-import java.util.UUID;
 
-import co.blustor.gatekeeper.net.SerialPortFTPClient;
-import co.blustor.gatekeeper.net.ApacheFTPClient;
-import co.blustor.gatekeeper.serialport.SerialPortMultiplexer;
+import co.blustor.gatekeeper.net.SerialPortFTPClientFactory;
 import co.blustor.gatekeeper.data.FileVault;
 import co.blustor.gatekeeper.data.LocalFilestore;
 
@@ -25,7 +17,7 @@ import static android.os.Environment.getExternalStorageDirectory;
 public class Configuration {
     private static final String TAG = "Configuration";
     private static final String FILE_SEPARATOR = System.getProperty("file.separator");
-    public static final UUID BLUETOOTH_SPP_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+    public static final String PAIRED_BLUETOOTH_DEVICE_NAME = "BLUSTOR";
 
     protected static File getAppDataPath() {
         String basePath = getExternalStorageDirectory().getAbsolutePath();
@@ -38,35 +30,18 @@ public class Configuration {
     }
 
     public static RemoteFilestore getRemoteFilestore() {
-        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
-        BluetoothDevice device = null;
-        for(BluetoothDevice d : pairedDevices) {
-            if(d.getName().equals("BLUSTOR")) {
-                device = d;
-            }
-        }
-
         try {
-            BluetoothSocket socket = device.createRfcommSocketToServiceRecord(BLUETOOTH_SPP_UUID);
-            socket.connect();
-
-            OutputStream os = socket.getOutputStream();
-
-            InputStream is = socket.getInputStream();
-
-            SerialPortMultiplexer multiplexer = new SerialPortMultiplexer(is, os);
-
-
-            co.blustor.gatekeeper.net.FTPClient ftpClient = new SerialPortFTPClient(multiplexer);
+            SerialPortFTPClientFactory factory = new SerialPortFTPClientFactory();
+            co.blustor.gatekeeper.net.FTPClient ftpClient = factory.createFromPairedBluetoothDevice(PAIRED_BLUETOOTH_DEVICE_NAME);
             //co.blustor.gatekeeper.net.FTPClient ftpClient = new ApacheFTPClient();
+
             FTPFilestoreClient client = new FTPFilestoreClient(ftpClient);
             return new RemoteFilestore(client);
         } catch (IOException e) {
+            Log.e(TAG, "Error attempting to create FTPClient.");
             e.printStackTrace();
+            return null;
         }
-
-        return null;
     }
 
     public static FileVault getFileVault() {
