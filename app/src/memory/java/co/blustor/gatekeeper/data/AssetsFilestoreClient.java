3321cc33026a67b2fbd950564c0791a51dab8d42
Jason <jgoodman@8thlight.com>
2015-11-06 13:47:31 -0600

    Implement delete file and folder for AssetsFilestoreClient

--- app/src/memory/java/co/blustor/gatekeeper/data/AssetsFilestoreClient.java
+++ app/src/memory/java/co/blustor/gatekeeper/data/AssetsFilestoreClient.java
@@ -1,16 +1,13 @@
 package co.blustor.gatekeeper.data;
 
-import android.content.Context;
-import android.util.Log;
-
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.String;
 import java.util.ArrayList;
 import java.util.List;
 
-import co.blustor.gatekeeper.Application;
 import co.blustor.gatekeeper.util.FileUtils;
 
 public class AssetsFilestoreClient implements RemoteFilestoreClient {
@@ -22,13 +19,15 @@ public class AssetsFilestoreClient implements RemoteFilestoreClient {
     @Override
     public List<VaultFile> listFiles(String targetPath) throws IOException {
         ArrayList<VaultFile> files = new ArrayList<>();
-        Context context = Application.getAppContext();
-        String[] filenames = context.getAssets().list(targetPath);
-        for (String name : filenames) {
-            if (name.contains(".")) {
-                files.add(new AssetVaultFile(targetPath, name, VaultFile.Type.FILE));
-            } else {
-                files.add(new AssetVaultFile(targetPath, name, VaultFile.Type.DIRECTORY));
+        File file = new File(DATA_PATH, targetPath);
+        if (file.exists()) {
+            String[] filenames = file.list();
+            for (String name : filenames) {
+                if (name.contains(".")) {
+                    files.add(new AssetVaultFile(targetPath, name, VaultFile.Type.FILE));
+                } else {
+                    files.add(new AssetVaultFile(targetPath, name, VaultFile.Type.DIRECTORY));
+                }
             }
         }
         return files;
@@ -36,8 +35,8 @@ public class AssetsFilestoreClient implements RemoteFilestoreClient {
 
     @Override
     public File downloadFile(VaultFile vaultFile) throws IOException {
-        Context context = Application.getAppContext();
-        InputStream inputStream = context.getAssets().open(vaultFile.getRemotePath());
+        File remoteFile = new File(DATA_PATH, vaultFile.getRemotePath());
+        InputStream inputStream = new FileInputStream(remoteFile);
         File targetFile = vaultFile.getLocalPath();
         FileUtils.writeStreamToFile(inputStream, targetFile);
         return targetFile;
@@ -51,6 +50,18 @@ public class AssetsFilestoreClient implements RemoteFilestoreClient {
         }
         FileUtils.writeStreamToFile(localFile, targetFile);
         return true;
+    }
+
+    @Override
+    public boolean deleteFile(String fileAbsolutePath) throws IOException {
+        File targetFile = new File(DATA_PATH, fileAbsolutePath);
+        return targetFile.delete();
+    }
+
+    @Override
+    public boolean removeDirectory(String directoryAbsolutePath) throws IOException {
+        File targetDirectory = new File(DATA_PATH, directoryAbsolutePath);
+        return targetDirectory.delete();
     }
 
     @Override
