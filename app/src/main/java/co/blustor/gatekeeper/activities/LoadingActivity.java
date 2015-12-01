0e2553b2410059bff6905dbda6b5d36aae35899e
Jeremy Neander <subjectego@gmail.com>
2015-12-01 13:23:13 -0600

    Move Initialization into InitializationFragment

--- app/src/main/java/co/blustor/gatekeeper/activities/LoadingActivity.java
+++ app/src/main/java/co/blustor/gatekeeper/activities/LoadingActivity.java
@@ -1,145 +1,33 @@
 package co.blustor.gatekeeper.activities;
 
-import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.util.Log;
 
 import co.blustor.gatekeeper.R;
-import co.blustor.gatekeeper.biometrics.Environment;
-import co.blustor.gatekeeper.biometrics.FaceCapture;
-import co.blustor.gatekeeper.data.Datastore;
-import co.blustor.gatekeeper.data.DroidDatastore;
 import co.blustor.gatekeeper.fragments.InitializationFragment;
 
-public class LoadingActivity extends BaseActivity implements Environment.InitializationListener {
+public class LoadingActivity extends BaseActivity {
     public static final String TAG = LoadingActivity.class.getSimpleName();
 
-    public static final int NANOS_IN_MILLIS = 1000000;
-    public static final long DELAY = NANOS_IN_MILLIS * 1000;
-
-    private long mLoadingStartTime;
-
-    private Fragment mInitializationFragment;
-    private AsyncTask<Void, Void, Void> mStartFaceTask = new LoadingTask();
-    private AsyncTask<Void, Void, Void> mStartFaceAuthTask = new LoadingTask();
+    private InitializationFragment mInitializationFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
-        setContentFragment();
-
-        Environment env = Environment.getInstance(this);
-
-        mLoadingStartTime = System.nanoTime();
-        env.initialize(this);
+        setInitializationFragment();
     }
 
     @Override
     public void onBackPressed() {
-        mStartFaceTask.cancel(true);
-        mStartFaceAuthTask.cancel(true);
+        mInitializationFragment.cancel();
         super.onBackPressed();
     }
 
-    @Override
-    public void onLicensesObtained() {
-        startFaceCapture();
-    }
-
-    private void startFaceCapture() {
-        final FaceCapture faceCapture = FaceCapture.getInstance();
-
-        mStartFaceTask = new LoadingTask() {
-            @Override
-            protected Void doInBackground(Void... params) {
-                if (!isCancelled()) {
-                    faceCapture.start();
-                }
-                return null;
-            }
-
-            @Override
-            protected void onPostExecute(Void aVoid) {
-                super.onPostExecute(aVoid);
-                if (!isCancelled()) {
-                    mStartFaceTask = new LoadingTask();
-                    onFaceCaptureStarted();
-                }
-            }
-
-            @Override
-            protected void onCancelled() {
-                super.onCancelled();
-                faceCapture.discard();
-            }
-        }.execute();
-    }
-
-    private void onFaceCaptureStarted() {
-        long elapsed = System.nanoTime() - mLoadingStartTime;
-        if (elapsed < DELAY) {
-            startFaceAuthWithDelay(elapsed);
-        } else {
-            startFaceAuth();
-        }
-    }
-
-    private void startFaceAuth() {
-        Datastore datastore = DroidDatastore.getInstance(this);
-        if (datastore.hasTemplate()) {
-            startActivity(new Intent(this, AuthenticationActivity.class));
-        } else {
-            startActivity(new Intent(this, EnrollmentActivity.class));
-        }
-        finish();
-    }
-
-    private void startFaceAuthWithDelay(final long elapsedTime) {
-        mStartFaceAuthTask = new LoadingTask() {
-            @Override
-            protected Void doInBackground(Void... params) {
-                if (!isCancelled()) {
-                    try {
-                        Thread.sleep((DELAY - elapsedTime) / NANOS_IN_MILLIS);
-                    } catch (InterruptedException e) {
-                        Log.w(TAG, "Artificial Delay Interrupted", e);
-                    }
-                }
-                return null;
-            }
-
-            @Override
-            protected void onPostExecute(Void aVoid) {
-                if (!isCancelled()) {
-                    super.onPostExecute(aVoid);
-                    mStartFaceTask = new LoadingTask();
-                    startFaceAuth();
-                }
-            }
-
-            @Override
-            protected void onCancelled() {
-                super.onCancelled();
-                FaceCapture.getInstance().discard();
-            }
-        }.execute();
-    }
-
-    private class LoadingTask extends AsyncTask<Void, Void, Void> {
-        @Override
-        protected Void doInBackground(Void... params) {
-            return null;
-        }
-    }
-
-    private void setContentFragment() {
+    private void setInitializationFragment() {
         FragmentManager fm = getSupportFragmentManager();
-        mInitializationFragment = fm.findFragmentByTag(InitializationFragment.TAG);
+        mInitializationFragment = (InitializationFragment) fm.findFragmentByTag(InitializationFragment.TAG);
 
         if (mInitializationFragment == null) {
             mInitializationFragment = new InitializationFragment();
--- app/src/main/java/co/blustor/gatekeeper/fragments/InitializationFragment.java
+++ app/src/main/java/co/blustor/gatekeeper/fragments/InitializationFragment.java
@@ -1,25 +1,143 @@
 package co.blustor.gatekeeper.fragments;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import co.blustor.gatekeeper.R;
+import co.blustor.gatekeeper.activities.AuthenticationActivity;
+import co.blustor.gatekeeper.activities.EnrollmentActivity;
+import co.blustor.gatekeeper.biometrics.Environment;
+import co.blustor.gatekeeper.biometrics.FaceCapture;
+import co.blustor.gatekeeper.data.Datastore;
+import co.blustor.gatekeeper.data.DroidDatastore;
 
-public class InitializationFragment extends Fragment {
+public class InitializationFragment extends Fragment implements Environment.InitializationListener {
     public static final String TAG = InitializationFragment.class.getSimpleName();
+
+    public static final int NANOS_IN_MILLIS = 1000000;
+    public static final long DELAY = NANOS_IN_MILLIS * 1000;
+
+    private long mLoadingStartTime;
+
+    private AsyncTask<Void, Void, Void> mStartFaceInitTask = new LoadingTask();
+    private AsyncTask<Void, Void, Void> mStartFaceAuthTask = new LoadingTask();
 
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRetainInstance(true);
+
+        Environment env = Environment.getInstance(getActivity());
+
+        mLoadingStartTime = System.nanoTime();
+        env.initialize(this);
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_initialization, container, false);
+    }
+
+    @Override
+    public void onLicensesObtained() {
+        startFaceCapture();
+    }
+
+    private void startFaceCapture() {
+        final FaceCapture faceCapture = FaceCapture.getInstance();
+
+        mStartFaceInitTask = new LoadingTask() {
+            @Override
+            protected Void doInBackground(Void... params) {
+                if (!isCancelled()) {
+                    faceCapture.start();
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                super.onPostExecute(aVoid);
+                if (!isCancelled()) {
+                    mStartFaceInitTask = new LoadingTask();
+                    onFaceCaptureStarted();
+                }
+            }
+
+            @Override
+            protected void onCancelled() {
+                super.onCancelled();
+                faceCapture.discard();
+            }
+        }.execute();
+    }
+
+    private void onFaceCaptureStarted() {
+        long elapsed = System.nanoTime() - mLoadingStartTime;
+        if (elapsed < DELAY) {
+            startFaceAuthWithDelay(elapsed);
+        } else {
+            startFaceAuth();
+        }
+    }
+
+    private void startFaceAuth() {
+        Datastore datastore = DroidDatastore.getInstance(getActivity());
+        if (datastore.hasTemplate()) {
+            startActivity(new Intent(getActivity(), AuthenticationActivity.class));
+        } else {
+            startActivity(new Intent(getActivity(), EnrollmentActivity.class));
+        }
+        getActivity().finish();
+    }
+
+    private void startFaceAuthWithDelay(final long elapsedTime) {
+        mStartFaceAuthTask = new LoadingTask() {
+            @Override
+            protected Void doInBackground(Void... params) {
+                if (!isCancelled()) {
+                    try {
+                        Thread.sleep((DELAY - elapsedTime) / NANOS_IN_MILLIS);
+                    } catch (InterruptedException e) {
+                        Log.w(TAG, "Artificial Delay Interrupted", e);
+                    }
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                if (!isCancelled()) {
+                    super.onPostExecute(aVoid);
+                    mStartFaceInitTask = new LoadingTask();
+                    startFaceAuth();
+                }
+            }
+
+            @Override
+            protected void onCancelled() {
+                super.onCancelled();
+                FaceCapture.getInstance().discard();
+            }
+        }.execute();
+    }
+
+    public void cancel() {
+        mStartFaceInitTask.cancel(true);
+        mStartFaceAuthTask.cancel(true);
+    }
+
+    private class LoadingTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            return null;
+        }
     }
 }
