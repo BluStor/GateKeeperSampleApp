3980cca9a62ef9f0002321813aa49fccf93f7b36
Jeremy Neander <subjectego@gmail.com>
2015-12-14 14:42:07 -0600

    Extract RequestPairDialogFragment

--- app/src/main/java/co/blustor/gatekeeper/fragments/InitializationFragment.java
+++ app/src/main/java/co/blustor/gatekeeper/fragments/InitializationFragment.java
@@ -1,17 +1,13 @@
 package co.blustor.gatekeeper.fragments;
 
-import android.app.Dialog;
 import android.bluetooth.BluetoothAdapter;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AlertDialog;
 import android.util.Log;
-import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -88,11 +84,6 @@ public class InitializationFragment extends Fragment implements Environment.Init
     private void requestBluetooth() {
         Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
         startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-    }
-
-    private void requestPairWithCard() {
-        Intent enableBtIntent = new Intent(android.provider.Settings.ACTION_BLUETOOTH_SETTINGS);
-        startActivityForResult(enableBtIntent, REQUEST_CARD_PAIR);
     }
 
     private void initializeFaceCapture() {
@@ -191,46 +182,5 @@ public class InitializationFragment extends Fragment implements Environment.Init
         }
     }
 
-    private DialogFragment mRequestPairDialog = new DialogFragment() {
-        @Override
-        public void onDestroyView() {
-            if (getDialog() != null && getRetainInstance()) {
-                getDialog().setDismissMessage(null);
-            }
-            super.onDestroyView();
-        }
-
-        @Override
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
-            setRetainInstance(true);
-            setCancelable(false);
-            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-            builder.setTitle(getString(R.string.gk_pair_requested_title))
-                   .setMessage(getString(R.string.gk_pair_requested_message))
-                   .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
-                       @Override
-                       public void onClick(DialogInterface dialog, int which) {
-                           requestPairWithCard();
-                       }
-                   })
-                   .setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
-                       @Override
-                       public void onClick(DialogInterface dialog, int which) {
-                           getActivity().finish();
-                       }
-                   });
-            builder.setOnKeyListener(new DialogInterface.OnKeyListener() {
-                @Override
-                public boolean onKey(DialogInterface dialog, int keyCode, KeyEvent event) {
-                    if (keyCode == KeyEvent.KEYCODE_BACK) {
-                        getActivity().finish();
-                        return true;
-                    } else {
-                        return false;
-                    }
-                }
-            });
-            return builder.create();
-        }
-    };
+    private DialogFragment mRequestPairDialog = new RequestPairDialogFragment();
 }
---
+++ app/src/main/java/co/blustor/gatekeeper/fragments/RequestPairDialogFragment.java
@@ -0,0 +1,63 @@
+package co.blustor.gatekeeper.fragments;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+import android.view.KeyEvent;
+
+import co.blustor.gatekeeper.R;
+
+public class RequestPairDialogFragment extends DialogFragment {
+    public static final String TAG = RequestPairDialogFragment.class.getSimpleName();
+
+    private static final int REQUEST_CARD_PAIR = 1;
+
+    @Override
+    public void onDestroyView() {
+        if (getDialog() != null && getRetainInstance()) {
+            getDialog().setDismissMessage(null);
+        }
+        super.onDestroyView();
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        setRetainInstance(true);
+        setCancelable(false);
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle(getString(R.string.gk_pair_requested_title))
+               .setMessage(getString(R.string.gk_pair_requested_message))
+               .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                   @Override
+                   public void onClick(DialogInterface dialog, int which) {
+                       requestPairWithCard();
+                   }
+               })
+               .setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                   @Override
+                   public void onClick(DialogInterface dialog, int which) {
+                       getActivity().finish();
+                   }
+               });
+        builder.setOnKeyListener(new DialogInterface.OnKeyListener() {
+            @Override
+            public boolean onKey(DialogInterface dialog, int keyCode, KeyEvent event) {
+                if (keyCode == KeyEvent.KEYCODE_BACK) {
+                    getActivity().finish();
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        });
+        return builder.create();
+    }
+
+    private void requestPairWithCard() {
+        Intent enableBtIntent = new Intent(android.provider.Settings.ACTION_BLUETOOTH_SETTINGS);
+        startActivityForResult(enableBtIntent, REQUEST_CARD_PAIR);
+    }
+};
